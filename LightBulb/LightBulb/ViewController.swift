e770448c7e159c8596d988c1007b9f9e8033ddf8
Jhantelle Belleza <jhantellebelleza@Jhantelles-MacBook-Pro.local>
2016-08-31 18:13:17 -0400

    Completed Program

--- LightBulb/LightBulb/ViewController.swift
+++ LightBulb/LightBulb/ViewController.swift
@@ -10,17 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var lightBulb: UIView!
-
+    @IBOutlet weak var colorSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Change background color of lightBulb view to red
+        lightBulb.backgroundColor = "red".color
+        
     }
 
+    
+    
+    
     func changeColor(to color: UIColor) {
         // TODO: Change background color to "color"
+        lightBulb.backgroundColor = color
     }
+    
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
         // TODO: Change background color when segmented control changes
-    }
+        switch colorSegment.selectedSegmentIndex{
+        case 0: //Red
+            changeColor(to: UIColor.redColor())
+        case 1: //Yellow
+            changeColor(to: UIColor.yellowColor())
+        case 2: //Blue
+            changeColor(to: UIColor.blueColor())
+        case 3: //Green
+            changeColor(to: UIColor.greenColor())
+        default:
+            changeColor(to: UIColor.blackColor())
+        }
+}
 }
